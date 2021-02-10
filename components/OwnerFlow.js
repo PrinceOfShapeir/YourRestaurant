46b8bd112af25f5059f211d9d6becfd03769aae2
Sean Farley <somethingfarley@gmail.com>
2021-02-10 17:43:50 -0500

    added register and login buttons to ownerFlow

--- components/OwnerFlow.js
+++ components/OwnerFlow.js
@@ -8,8 +8,17 @@ function ownerFlow (props) {
         <>
         <Text>Welcome Owners.</Text>
         <Text>Would you like to start a new restaurant?</Text>
+        <Button
+        title="Register New Store"
+        onPress={()=>console.log("new store registration")} 
+        />
         <Text>Login and manage an existing one?</Text>
+        <Button
+        title="Login"
+        onPress={()=>console.log("Login Request")} 
+        />
         </>
+        
         )
 }
 
